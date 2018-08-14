aff9153fe58808c4463c12f0fdbaa198de08c92d
Jeroen <druwe.jeroen@gmail.com>
2018-08-14 13:19:20 +0200

    Refactor processor

--- src/main/java/translator/model/JsonTranslationFile.java
+++ src/main/java/translator/model/JsonTranslationFile.java
@@ -1,14 +1,11 @@
 package translator.model;
 
-import com.fasterxml.jackson.core.util.DefaultIndenter;
-import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
 import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NotNull;
 import translator.exception.InvalidFileException;
 import translator.exception.InvalidJsonException;
 
@@ -24,13 +21,7 @@ public class JsonTranslationFile extends TranslationFile {
     public String addTranslation(String key, String value, boolean alphabetize) throws IOException, InvalidFileException, InvalidJsonException {
 
         validateFile();
-
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.enable(SerializationFeature.INDENT_OUTPUT);
-
-        if (alphabetize) {
-            mapper.enable(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS);
-        }
+        ObjectMapper mapper = buildMapper(alphabetize);
 
         JsonNode jsonNode = mapper.readTree(getContent());
         ((ObjectNode) jsonNode).put(key, value);
@@ -38,16 +29,17 @@ public class JsonTranslationFile extends TranslationFile {
         String s = mapper.writeValueAsString(obj);
 
         return s.replaceAll("\" :", "\":");
+    }
 
-//
-//        try {
-//            JsonObject jsonObject = parsed.getAsJsonObject();
-//            jsonObject.addProperty(key, value);
-//            Gson gson = new GsonBuilder().setPrettyPrinting().create();
-//            return gson.toJson(jsonObject);
-//        } catch (IllegalStateException e) {
-//            throw new InvalidJsonException(getVirtualFile().getName() + " is not a valid JSON file.");
-//        }
+    @NotNull
+    private ObjectMapper buildMapper(boolean alphabetize) {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+        if (alphabetize) {
+            mapper.enable(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS);
+        }
+        return mapper;
     }
 
     private void validateFile() throws InvalidFileException {
