a54a08f205b0b91d0eb38c4d82c3bf23e513963a
Jeroen <druwe.jeroen@gmail.com>
2018-08-20 08:56:07 +0200

    Finishing 0.1.3 release

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
-Structure example:
-
-* translations (directory)
-    * nl.json
-    * fr.json
-    * en.json
-    * de.json
-    * other (directory)
-    
-The json files will be taken into account.
+# IntelliJ AEM Translator
+This plugin will make your life as an AEM developer a bit easier. Adding new translations is now possible in a breeze. The plugin will translate to all your available languages.
+![translator-demo](https://i.imgur.com/6vvgMJI.gif)
--- src/test/java/translator/model/JsonTranslationFileTest.java
+++ src/test/java/translator/model/JsonTranslationFileTest.java
@@ -1,5 +1,6 @@
 package translator.model;
 
+import com.intellij.mock.MockVirtualFile;
 import org.junit.Test;
 import translator.exception.InvalidFileException;
 import translator.exception.InvalidJsonException;
@@ -14,7 +15,7 @@ public class JsonTranslationFileTest {
     public void addTranslation_existing_updatedJson() throws IOException, InvalidFileException, InvalidJsonException {
 
         String json = "{ \"account.account\" : \"Account\", \"account.activation.already.exists\" : \"Gelieve je aan te melden via het accountmenu.\" }";
-        JsonTranslationFile jsonTranslationFile = new JsonTranslationFile(new MockVirtualFile(json, true));
+        JsonTranslationFile jsonTranslationFile = new JsonTranslationFile(buildMockFile(json));
         String newContent = jsonTranslationFile.addTranslation("account.account", "Account new translation", false);
         assertEquals("{\n" +
                 "  \"account.account\": \"Account new translation\",\n" +
@@ -26,7 +27,7 @@ public class JsonTranslationFileTest {
     public void addTranslation_nonExistingAlphabetizeRequested_updatedJson() throws IOException, InvalidFileException, InvalidJsonException {
 
         String json = "{\"credit: s\": 10, \"account.account\": \"account\"}";
-        JsonTranslationFile jsonTranslationFile = new JsonTranslationFile(new MockVirtualFile(json, true));
+        JsonTranslationFile jsonTranslationFile = new JsonTranslationFile(buildMockFile(json));
         String newContent = jsonTranslationFile.addTranslation("between", "This translation should be added in between", true);
         assertEquals("{\n" +
                 "  \"account.account\": \"account\",\n" +
@@ -39,13 +40,17 @@ public class JsonTranslationFileTest {
     public void addTranslation_nonExisting_updatedJson() throws IOException, InvalidFileException, InvalidJsonException {
 
         String json = "{\"credit: s\": 10, \"account.account\": \"account\"}";
-        JsonTranslationFile jsonTranslationFile = new JsonTranslationFile(new MockVirtualFile(json, true));
+        JsonTranslationFile jsonTranslationFile = new JsonTranslationFile(buildMockFile(json));
         String newContent = jsonTranslationFile.addTranslation("between", "This translation should be added in between", false);
         assertEquals("{\n" +
                 "  \"credit: s\": 10,\n" +
                 "  \"account.account\": \"account\",\n" +
                 "  \"between\": \"This translation should be added in between\"\n" +
                 "}", newContent);
+    }
+
+    private MockVirtualFile buildMockFile(String content) {
+        return new MockVirtualFile("mock.json", content);
     }
 
 }
